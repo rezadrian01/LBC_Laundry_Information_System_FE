b5ee3a49086668a2b385a634e048b8d37cd79087
rezadrian01 <ahmadadrian324@gmail.com>
2024-10-25 18:41:41 +0700

    finishing tailwind colors config

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='bg-primary-pink-800 text-primary-pink-100'>Login</div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary-pink': {
+        100: '#fff6f6',
+        200: '#ffdfdf',
+        300: '#f87aac',
+        400: '#f875ac',
+        800: '#e72473'
+      },
+      'red-trash': '#ef4444',
+      'gradient-purple': '#9370db',
+      'gradient-green': '#40e0d0',
+      'gradient-blue': '#007bff',
+    },
     extend: {},
   },
   plugins: [],
