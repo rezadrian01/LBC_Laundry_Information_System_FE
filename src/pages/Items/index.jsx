548c4101dd7cbbcb9b02b96283916659b0975275
rezadrian01 <ahmadadrian324@gmail.com>
2024-11-20 09:46:04 +0700

    feat: create pagination functionality on fetched items

--- src/pages/Items/index.jsx
+++ src/pages/Items/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
 import DefaultLayout from '@layouts/Default';
 import Crud from '@mods/Crud';
@@ -15,14 +16,35 @@ const Items = () => {
     const navigate = useNavigate();
     const { isLoading: loadAuthData } = useAuth();
     const [searchInput, setSearchInput] = useState("");
-    const { data: itemList, isLoading: isLoadingItemList, isError: isErrorItemList, refetch: refetchItemList } = useQuery({
-        queryKey: ['items', { searchInput: searchInput.length >= 2 ? searchInput : "" }],
+    const [itemList, setItemList] = useState([]);
+    const [hasNextPage, setHasNextPage] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const { ref, inView } = useInView();
+
+    const { data, isLoading: isLoadingItemList, isError: isErrorItemList, refetch: refetchItemList } = useQuery({
+        queryKey: ['items', { searchInput: searchInput.length >= 2 ? searchInput : "" }, { currentPage }],
         queryFn: async () => {
-            let requestUrl = `item/group`;
+            let requestUrl = `item/group?page=${currentPage}`;
             if (searchInput.length >= 2) {
                 requestUrl = `item/search/${searchInput}`;
             }
             const response = await apiInstance(requestUrl);
+            const { data } = response;
+            setItemList(prev => {
+                if (searchInput.length >= 2) {
+                    setCurrentPage(1);
+                    return [...data.data];
+                } else {
+                    setHasNextPage(data.data.hasNextPage);
+                    // if this first page then reset item list
+                    if (currentPage === 1) {
+                        return [...data.data.itemList];
+                    }
+                    // else push to array
+                    return [...prev, ...data.data.itemList];
+                }
+            })
+
             return response.data.data;
         },
         enabled: !loadAuthData
@@ -32,7 +54,13 @@ const Items = () => {
         if (searchInput.length >= 2) {
             refetchItemList();
         }
-    }, [searchInput])
+    }, [searchInput]);
+
+    useEffect(() => {
+        if (hasNextPage && !isLoadingItemList && inView) {
+            setCurrentPage(prev => prev + 1);
+        }
+    }, [inView])
 
     const keys = ["_", "name", "Original (Lipat)", "Gantung", "Dry Clean"];
     const handleCreateItem = () => {
@@ -42,7 +70,8 @@ const Items = () => {
     return (
         <DefaultLayout>
             <Sidebar />
-            {<Crud isPending={isLoadingItemList} searchInput={searchInput} setSearchInput={setSearchInput} onCreate={handleCreateItem} keys={keys} isItemList tableHeader={TABLE_HEADER} tableContent={itemList} />}
+            {<Crud isPending={itemList?.length === 0} searchInput={searchInput} setSearchInput={setSearchInput} onCreate={handleCreateItem} keys={keys} isItemList tableHeader={TABLE_HEADER} tableContent={itemList} />}
+            <div ref={ref} />
             <Footer backToDashboard hasNext={false} />
         </DefaultLayout>
     );
